56a0b2469995abcae3f4caab65e93bc48af23786
Rohtash Lakra <rslakra@users.noreply.github.com>
2023-10-28 18:32:54 -0700

    code refactored and moved to respective project

---
+++ src/main/java/com/devamatre/appsuite/questions/leetcode/LC20ValidParentheses.java
@@ -0,0 +1,46 @@
+package com.devamatre.appsuite.questions.leetcode;
+
+/**
+ * https://leetcode.com/problems/valid-parentheses/
+ * <p>
+ * <p>
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is
+ * valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every
+ * close bracket has a corresponding open bracket of the same type.
+ * <p>
+ * <p>
+ * Example 1:
+ * <pre>
+ * Input: s = "()"
+ * Output: true
+ * </pre>
+ * <p>
+ * Example 2:
+ * <pre>
+ * Input: s = "()[]{}"
+ * Output: true
+ * </pre>
+ * Example 3:
+ * <pre>
+ * Input: s = "(]"
+ * Output: false
+ * </pre>
+ *
+ * @author Rohtash Lakra
+ * @created 10/13/23 11:48 AM
+ */
+public class LC20ValidParentheses {
+
+    /**
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        return false;
+    }
+
+}
---
+++ src/main/java/com/devamatre/appsuite/questions/leetcode/LC32LongestValidParentheses.java
@@ -0,0 +1,32 @@
+package com.devamatre.appsuite.questions.leetcode;
+
+/**
+ * Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses
+ * substring
+ * .
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()".
+ * Example 2:
+ *
+ * Input: s = ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()".
+ * Example 3:
+ *
+ * Input: s = ""
+ * Output: 0
+ *
+ * @author Rohtash Lakra
+ * @created 10/13/23 11:53 AM
+ */
+public class LC32LongestValidParentheses {
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
+}
